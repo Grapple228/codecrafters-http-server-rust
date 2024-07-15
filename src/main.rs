b5435d2e03405f12d16de50c033cb9fd31b675e8
Grapple228 <greenapple228882@yandex.ru>
2024-07-15 16:24:52 +0700

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,17 @@
 // Uncomment this block to pass the first stage
-use std::net::TcpListener;
+use std::{io::{BufWriter, Write}, net::{TcpListener, TcpStream}};
+
+fn handle_client(mut stream: TcpStream){
+    let response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    let mut writer = BufWriter::new(&mut stream);
+    if writer.write_all(response.as_bytes()).is_err(){
+        println!("Failed to response to stream!");
+    }
+    if writer.flush().is_err(){
+        println!("Failed to flush stream!");
+    }
+}
 
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
@@ -11,6 +23,7 @@ fn main() {
         match stream {
             Ok(_stream) => {
                 println!("accepted new connection");
+                handle_client(_stream);
             }
             Err(e) => {
                 println!("error: {}", e);
