7e17a53deb7c262f5ab4d84ab6f491acf426068a
Grapple <greenapple228882@yandex.ru>
2024-07-15 18:20:35 +0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,43 @@
 // Uncomment this block to pass the first stage
-use std::{io::{BufWriter, Write}, net::{TcpListener, TcpStream}};
+use std::{
+    io::{BufRead, BufReader, BufWriter, Write}, 
+    net::{TcpListener, TcpStream}, vec};
+
+use itertools::Itertools;
+
+fn split_request(reader: BufReader<&mut TcpStream>) -> Vec<String> {
+    reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect()
+}
 
 fn handle_client(mut stream: TcpStream){
-    let response = "HTTP/1.1 200 OK\r\n\r\n";
+    // READ REQUEST
+    let reader = BufReader::new(&mut stream);
+    let request: Vec<_> = split_request(reader);
+    
+    println!("Incoming request: {request:#?}");
+
+    let request_line = &request[0];
+
+    let first_line_parts: Vec<&str> = request_line.split(" ").collect();
+    
+    let (response_status, response_message) = match first_line_parts[1] {
+        "/" | "/index.html" => ("200", "OK"),
+        _ => ("404", "Not Found")
+    };
+
+    let status_line = format!("HTTP/1.1 {response_status} {response_message}");
+
+    let mut contents: String = request.into_iter().collect();
+    contents = "".to_string();
+    let length = contents.len();
+
+    // WRITING RESPONSE
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
 
     let mut writer = BufWriter::new(&mut stream);
     if writer.write_all(response.as_bytes()).is_err(){
