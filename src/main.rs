a2807952f2bc3f14f6c5c195ab9043c70ab3eecd
Grapple <greenapple228882@yandex.ru>
2024-07-15 19:18:56 +0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 // Uncomment this block to pass the first stage
 use std::{
-    io::{BufRead, BufReader, BufWriter, Write}, 
-    net::{TcpListener, TcpStream}, vec};
-
-use itertools::Itertools;
+        io::{BufRead, BufReader, BufWriter, Write}, 
+        net::{TcpListener, TcpStream}};
 
 fn split_request(reader: BufReader<&mut TcpStream>) -> Vec<String> {
     reader
@@ -13,31 +11,43 @@ fn split_request(reader: BufReader<&mut TcpStream>) -> Vec<String> {
         .collect()
 }
 
+fn get_response(segments: &[&str]) -> (String, String, String) {
+    let _last: &str = &segments.last().unwrap_or(&"");
+    
+    let (response_status, status_message, response_contents) = match &segments {        
+        [] | ["index.html"] => ("200", "OK", ""), 
+        ["echo", _last] => ("200", "OK", *_last),
+        _ => ("404", "Not Found", "")
+    };
+
+    (response_status.to_string(), status_message.to_string(), response_contents.to_string())
+}
+
+fn get_segments(path: &str) -> Vec<&str> {
+    path.split('/').filter(|s| !s.is_empty()).collect()
+}
+
 fn handle_client(mut stream: TcpStream){
     // READ REQUEST
     let reader = BufReader::new(&mut stream);
     let request: Vec<_> = split_request(reader);
     
     println!("Incoming request: {request:#?}");
-
+    
     let request_line = &request[0];
 
     let first_line_parts: Vec<&str> = request_line.split(" ").collect();
-    
-    let (response_status, response_message) = match first_line_parts[1] {
-        "/" | "/index.html" => ("200", "OK"),
-        _ => ("404", "Not Found")
-    };
 
-    let status_line = format!("HTTP/1.1 {response_status} {response_message}");
+    let segments: Vec<&str> = get_segments(&first_line_parts[1]);
 
-    let mut contents: String = request.into_iter().collect();
-    contents = "".to_string();
-    let length = contents.len();
+    let (response_status, status_message, response_contents) = get_response(&segments); 
+
+    let status_line = format!("HTTP/1.1 {response_status} {status_message}");
+    let length = response_contents.len();
 
     // WRITING RESPONSE
     let response =
-        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+        format!("{status_line}\r\nContent-Type: text/plain\r\nContent-Length: {length}\r\n\r\n{response_contents}");
 
     let mut writer = BufWriter::new(&mut stream);
     if writer.write_all(response.as_bytes()).is_err(){
