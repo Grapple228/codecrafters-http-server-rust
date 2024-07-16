2211a4aab4d939e776059e56d3b2a12840304e6d
Grapple228 <greenapple228882@yandex.ru>
2024-07-16 21:40:07 +0700

    pass stage

--- src/main.rs
+++ src/main.rs
@@ -1,56 +1,157 @@
 // Uncomment this block to pass the first stage
 use std::{
-        io::{BufRead, BufReader, BufWriter, Write}, 
-        net::{TcpListener, TcpStream}};
+        fmt, io::{BufRead, BufReader, BufWriter, Write}, net::{TcpListener, TcpStream}};
 
-fn split_request(reader: BufReader<&mut TcpStream>) -> Vec<String> {
-    reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect()
+enum RequestType {
+    GET,
+    POST,
+    PUT,
+    DELETE,
+    UNKNOWN
 }
 
-fn get_response(segments: &[&str]) -> (String, String, String) {
-    let _last: &str = &segments.last().unwrap_or(&"");
+
+#[derive(Debug, Copy, Clone)]
+enum StatusCode{
+    OK= 200,
+    Bad= 400,
+    Unauthorized= 401,
+    NotFound= 404,    
+}
+
+impl fmt::Display for StatusCode {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{:?}", *self as u8)
+    }
+}
+
+struct Request{
+    request_type: RequestType,
+    content_type: String,
+    path: String,
+    protocol: String,
+    user_agent: String,
+    host: String,
+    accept: String,
+    content_length: i32
+}
+
+fn split_into(path: &str, splitter: char) -> Vec<&str> {
+    path.split(splitter).filter(|s| !s.is_empty()).collect()
+}
+
+impl Request {
+    fn split_request(reader: BufReader<&mut TcpStream>) -> Vec<String> {
+        reader
+            .lines()
+            .map(|result| result.unwrap())
+            .take_while(|line| !line.is_empty())
+            .collect()
+    }
+
+    /// Creates a new [`Request`].
+    fn new(reader: BufReader<&mut TcpStream>) -> Self{
+        let splitted_request =  Self::split_request(reader);
+        
+        println!("Processing request:\n{splitted_request:#?}");
+
+        let request_type: RequestType;
+        let path: String;
+        let protocol: String;
+
+        let splitted_first = split_into(&splitted_request[0], ' ');
+
+        request_type = match splitted_first[0].to_uppercase().as_str() {
+            "GET" => RequestType::GET,
+            "PUT" => RequestType::PUT,
+            "POST" => RequestType::POST,
+            "DELETE" =>  RequestType::DELETE,
+            _ => RequestType::UNKNOWN
+        };
+        path = splitted_first[1].to_owned();
+        protocol = splitted_first[2].to_owned();
+
+        let mut content_type: String = String::new();
+        let mut user_agent: String = String::new();
+        let mut accept: String = String::new();
+        let mut host: String = String::new();
+        let mut content_length: i32 = 0;
+
+        for line in &splitted_request{
+            let (property, value) = line.split_once(": ").unwrap_or(("",""));
+
+            match property {
+                "Content-Type" => content_type = value.to_owned(),
+                "User-Agent" => user_agent = value.to_owned(),
+                "Accept" => accept = value.to_owned(),
+                "Host" => host = value.to_owned(),
+                "Content-Length" => content_length = value.parse().unwrap_or(0),
+                _ => {}
+            }
+        };
+
+        let request = Self{
+            accept,
+            request_type,
+            path,
+            protocol,
+            user_agent,
+            host,
+            content_length,
+            content_type,
+        };
+        request
+    }
+}
+
+struct Response{
+    status_code: StatusCode,
+    status_message: String,
+    contents: String
+}
+
+impl Response {
+    fn new(status_code: StatusCode, status_message: String, contents: String) -> Self {
+        Self { status_code, status_message, contents }
+    }
+
+    fn to_string(&self) -> String{
+        let var_name: String = format!("HTTP/1.1 {0} {1}\r\nContent-Type: text/plain\r\nContent-Length: {2}\r\n\r\n{3}", 
+        self.status_code, 
+        self.status_message, 
+        self.contents.len(), 
+        self.contents);
+        var_name
+    }
+}
+
+fn process_request(request: Request) -> Response {    
+    let path_segments: &[&str] = &split_into(&request.path, '/');
     
-    let (response_status, status_message, response_contents) = match &segments {        
-        [] | ["index.html"] => ("200", "OK", ""), 
-        ["echo", _last] => ("200", "OK", *_last),
-        _ => ("404", "Not Found", "")
+    let (status_code, status_message, contents): (StatusCode, &str, &str) = match &path_segments {
+        [] | ["index.html"] => (StatusCode::OK, "OK", ""), 
+        ["user-agent"] => (StatusCode::OK, "OK", &request.user_agent),
+        ["echo", _echo] => (StatusCode::OK, "OK", *_echo),
+        _ => (StatusCode::NotFound, "Not Found", "")
     };
 
-    (response_status.to_string(), status_message.to_string(), response_contents.to_string())
-}
-
-fn get_segments(path: &str) -> Vec<&str> {
-    path.split('/').filter(|s| !s.is_empty()).collect()
+    Response::new(status_code, status_message.to_owned(), contents.to_owned())
 }
 
 fn handle_client(mut stream: TcpStream){
     // READ REQUEST
     let reader = BufReader::new(&mut stream);
-    let request: Vec<_> = split_request(reader);
     
-    println!("Incoming request: {request:#?}");
-    
-    let request_line = &request[0];
+    let request = Request::new(reader);
 
-    let first_line_parts: Vec<&str> = request_line.split(" ").collect();
+    let response = process_request(request);
 
-    let segments: Vec<&str> = get_segments(&first_line_parts[1]);
-
-    let (response_status, status_message, response_contents) = get_response(&segments); 
-
-    let status_line = format!("HTTP/1.1 {response_status} {status_message}");
-    let length = response_contents.len();
+    let response_string = response.to_string();
+    println!("Outcoming response:\n[\n{response_string}\n]");
 
     // WRITING RESPONSE
-    let response =
-        format!("{status_line}\r\nContent-Type: text/plain\r\nContent-Length: {length}\r\n\r\n{response_contents}");
-
     let mut writer = BufWriter::new(&mut stream);
-    if writer.write_all(response.as_bytes()).is_err(){
+    if writer.write_all(response_string.as_bytes()).is_err(){
         println!("Failed to response to stream!");
     }
     if writer.flush().is_err(){
